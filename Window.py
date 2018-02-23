ef65c47c4a41398d9c7c5187711955ef5b5e4659
Nathan Petrangelo <nathan@petrangelo.org>
2018-02-22 20:55:51 -0500

    Added comments and rearranged some stuff

--- Window.py
+++ Window.py
@@ -11,13 +11,13 @@ class Window(Frame):
 		# changing the title of our master widget      
 		self.master.title("3D Transformation")
 
+		# Define all the widgets
 		self.canvas = Canvas(self, width=768, height=561)
 		self.rect = self.canvas.create_rectangle(50, 25, 150, 75, fill="blue")
 
 		self.guiPanel = LabelFrame(self)
 
 		self.cubePanel = LabelFrame(self.guiPanel, text="Cube Controls")
-
 		self.vertex = Button(self.cubePanel, text="Vertex")
 		self.edge = Button(self.cubePanel, text="Edge")
 		self.face = Button(self.cubePanel, text="Face")
@@ -29,22 +29,22 @@ class Window(Frame):
 		self.front = Button(self.camPanel, text="Front")
 		self.camSlider = Scale(self.camPanel, from_=0, to=100, orient=HORIZONTAL, showvalue=False)
 		
-		# allowing the widget to take the full space of the root window
+		# Pack the panels, place the widgets
 		self.pack(fill=BOTH, expand=YES)
 		self.canvas.pack(fill=BOTH, expand=YES)
 
 		self.guiPanel.pack(fill=BOTH, expand=YES)
 
+		self.cubePanel.pack(fill=BOTH, expand=YES, side=LEFT)
 		self.vertex.place(relx=0.0, rely=0.5, anchor=W)
 		self.edge.place(relx=0.3, rely=0.5, anchor=CENTER)
 		self.face.place(relx=0.5, rely=0.5, anchor=CENTER)
 		self.camSlider.place(relx=1.0, rely=0.5, anchor=E)
-		self.cubePanel.pack(fill=BOTH, expand=YES, side=LEFT)
 
+		self.camPanel.pack(fill=BOTH, expand=YES, side=RIGHT)
 		self.top.place(relx=0.0, rely=0.5, anchor=W)
 		self.front.place(relx=0.5, rely=0.5, anchor=CENTER)
 		self.camSlider.place(relx=1.0, rely=0.5, anchor=E)
-		self.camPanel.pack(fill=BOTH, expand=YES, side=RIGHT)
 
 # 		placing the button on my window
 
