ef3116c3275af11b258b5d447747471c07d319a8
Nathan Petrangelo <nathan@petrangelo.org>
2018-02-28 14:03:00 -0500

    Added Camera.py, edited CubePoint.py

---
+++ Camera.py
@@ -0,0 +1,47 @@
+class Camera:
+
+	focalLength = 450
+	orthoZoom = 3
+
+	def __init__(self):
+		self.x = 0.0
+		self.y = 0.0
+		self.z = -150.0
+		self.phi = 0.0
+		self.factor = 1.0
+
+	def project(self, pt):
+		image = transform(pt)
+
+		projectionConstant = mix(focalLength / image.z, orthoZoom, self.factor)
+		return (int(image.x * projectionConstant), int(image.y * projectionConstant))
+
+	def tranform(self, point):
+		image = CubePoint(point.x, point.y, point.z)
+		axis = CubePoint(0.0, 0.0, 0.0)
+
+		image.rotateX(self.phi, axis)
+		image.translate(-self.x, -self.y, -self.z)
+		return image
+
+	def rotate(self, dPhi):
+		self.phi += dPhi
+
+	def setRotation(self, phi):
+		self.phi = phi
+
+	def translate(self, dx, dy, dz):
+		self.x += dx
+		self.y += dy
+		self.z += dz
+
+	def setPosition(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+
+	def setProjectionFactor(self, factor):
+		self.factor = factor
+
+	def mix(a, b, factor):
+		return (a*factor) + (b * (1 - factor))
--- CubePoint.py
+++ CubePoint.py
@@ -1,3 +1,5 @@
+import math
+
 class CubePoint:
 	def __init__(self, x, y, z):
 		self.x = x
@@ -12,5 +14,55 @@ class CubePoint:
 		self.y += dy
 		self.z += dz
 
+	def Draw(self, canvas, cam):
+		pt = cam.project(self)
+		canvas.create_oval(pt[0], pt[1], pt[0] + 8, pt[1] + 8, fill=green)
+
 	def getMatrix(self):
 		return [self.x, self.y, self.z]
+
+	def matrixMultiply(self, matrix):
+		pt = [0.0, 0.0, 0.0]
+
+		if (len(matrix[0] != 3)):
+			raise IndexError('matrix needs to be 3 dimensional')
+
+		for i in range(3):
+			for j in range(3):
+				pt[i] += self.getMatrix()[j] * matrix[i][j]
+
+	def rotateX(self, a, axis):
+		a *= math.pi / 180
+		matrix = [[1.0, 0.0, 0.0], [0.0, math.cos(a), -math.sin(a)], [0.0, math.sin(a), math.cos(a)]]
+
+		self.x -= axis.x
+		self.y -= axis.y
+		self.z -= axis.z
+		rotation = matrixMultiply(matrix)
+		self.x = rotation[0] + axis.x
+		self.y = rotation[1] + axis.y
+		self.z = rotation[2] + axis.z
+
+	def rotateY(self, a, axis):
+		a *= math.pi / 180
+		matrix = [[math.cos(a), 0.0, math.sin(a)], [0.0, 1.0, 0.0], [-math.sin(a), 0.0, math.cos(a)]]
+
+		self.x -= axis.x
+		self.y -= axis.y
+		self.z -= axis.z
+		rotation = matrixMultiply(matrix)
+		self.x = rotation[0] + axis.x
+		self.y = rotation[1] + axis.y
+		self.z = rotation[2] + axis.z
+
+	def rotateZ(self, a, axis):
+		a *= math.pi / 180
+		matrix = [[math.cos(a), -math.sin(a), 0.0], [math.sin(a), math.cos(a), 0.0], [0.0, 0.0, 1.0]]
+
+		self.x -= axis.x
+		self.y -= axis.y
+		self.z -= axis.z
+		rotation = matrixMultiply(matrix)
+		self.x = rotation[0] + axis.x
+		self.y = rotation[1] + axis.y
+		self.z = rotation[2] + axis.z
